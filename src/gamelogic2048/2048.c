43d8bb9512a0330d547c57d48a2f6c6b537a0173
Miguel <miguel.contreras@berkeley.edu>
2026-01-03 17:32:22 -0800

    intialized board

--- src/gamelogic2048/2048.c
+++ src/gamelogic2048/2048.c
@@ -1,6 +1,45 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
+
+
+
+void spawnTile (int board[4][4]) {
+    int emptyCount = 0;
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 4; j++) {
+           if (board[i][j] == 0) {
+            emptyCount++
+           }
+        }
+        
+    }
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 4; j++) {
+            printf("%d\t", board[i][j]); 
+        }
+        printf("\n");
+    }
+    return;
+
+
+}
+int board[4][4] = {{0,0,0,0},
+                  {0,0,0,0},
+                  {0,0,0,0},
+                  {0,0,0,0}
+                            };
 int main() {
-    printf("John Bruvold\n");
-    return 0;
+    printf("2048: The Beginning\n");
+    printf("-------------------\n");
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 4; j++) {
+            printf("%d\t", board[i][j]); 
+        }
+        printf("\n");
+    }
+    return 0; 
+
+
 }
